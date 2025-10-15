a826500dfea234fd8133e9bac8b29bc9dc49387a
wzh <289410265@qq.com>
2025-10-15 10:54:01 +0000

    add fisher-rao shell

--- eval/shell/main.sh
+++ eval/shell/main.sh
@@ -2,7 +2,7 @@
 ROOT=$(readlink -f $(dirname $0))
 cd $ROOT
 # 默认方法列表
-DEFAULT_METHODS=("mahalanobis" "mahalanobis_multi" "mahalanobis_plus")
+DEFAULT_METHODS=("mahalanobis" "mahalanobis_multi"  "mahalanobis_ood" "mahalanobis_plus" "igeood_plus") 
 
 # 检查是否提供了方法参数
 if [ $# -gt 0 ]; then
@@ -23,8 +23,12 @@ for METHOD in "${METHODS[@]}"; do
         ./run_mahalanobis.sh
     elif [ "$METHOD" == "mahalanobis_multi" ]; then
         ./run_mahalanobis_multi.sh
+    elif [ "$METHOD" == "mahalanobis_ood" ]; then
+        ./run_mahalanobis_ood.sh
     elif [ "$METHOD" == "mahalanobis_plus" ]; then
         ./run_mahalanobis_plus.sh
+    elif [ "$METHOD" == "igeood_plus" ]; then
+        ./run_igeood_plus.sh
     else
         echo "未知方法: $METHOD, 跳过"
     fi
